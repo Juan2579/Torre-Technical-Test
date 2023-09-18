bb063940402b2c0522ee843471dea628e55b77e5
Juan2579 <juandavidgualdronnavarro@gmail.com>
2023-09-18 15:08:58 -0500

    [ADD] Colors and fonts

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,6 +7,14 @@ module.exports = {
   ],
   theme: {
     extend: {
+      fontFamily: {
+        inter: ["var(--font-inter)"],
+        almarai: ["var(--font-almarai)"],
+      },
+      colors: {
+        "primary-purple": "#251f87",
+        "primary-blue": "#32cae4",
+      }
     },
   },
   plugins: [],
