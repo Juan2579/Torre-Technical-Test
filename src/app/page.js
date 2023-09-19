6b8577c8aa409f473ed97f9432297506ccc95ff5
Juan2579 <juandavidgualdronnavarro@gmail.com>
2023-09-19 00:01:27 -0500

    [ADD] Search people input

--- src/app/page.js
+++ src/app/page.js
@@ -1,9 +1,12 @@
 import { Hero } from "@/components/Hero";
+import { PeopleSearch } from "@/components/PeopleSearch";
 
 export default function Home() {
   return (
     <main className="w-full flex flex-col">
       <Hero />
+
+      <PeopleSearch />
     </main>
   )
 }
